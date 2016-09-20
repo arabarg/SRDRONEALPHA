19ece085669eb5e94bdcc39f260cb53d3f7194f0
arabarg <arabar@gsertel.com>
2016-09-20 09:51:16 +0200

    añadimos este archivo al commit anterior

--- viewer.h
+++ viewer.h
@@ -1,7 +1,32 @@
 #ifndef VIEWER_H
 #define VIEWER_H
 
+// Qt
 #include <QWidget>
+#include <QFileDialog>
+
+// Point Cloud Library
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/filters/filter.h>
+#include <pcl/visualization/pcl_visualizer.h>
+
+//Eigen
+
+#include <eigen3/Eigen/Geometry>
+
+// Boost
+#include <boost/math/special_functions/round.hpp>
+
+// Visualization Toolkit (VTK)
+#include <vtkRenderWindow.h>
+
+
+
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
 
 namespace Ui {
 class Viewer;
@@ -17,6 +42,59 @@ public:
 
 private:
     Ui::Viewer *ui;
+
+protected:
+    /** @brief The PCL visualizer object */
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_;
+
+    /** @brief The point cloud displayed */
+    PointCloudT::Ptr cloud_;
+
+    /** @brief 0 = x | 1 = y | 2 = z */
+    int filtering_axis_;
+
+    /** @brief Holds the color mode for @ref colorCloudDistances */
+    int color_mode_;
+
+    /** @brief Color point cloud on X,Y or Z axis using a Look-Up Table (LUT)
+     * Computes a LUT and color the cloud accordingly, available color palettes are :
+     *
+     *  Values are on a scale from 0 to 255:
+     *  0. Blue (= 0) -> Red (= 255), this is the default value
+     *  1. Green (= 0) -> Magenta (= 255)
+     *  2. White (= 0) -> Red (= 255)
+     *  3. Grey (< 128) / Red (> 128)
+     *  4. Blue -> Green -> Red (~ rainbow)
+     *
+     * @warning If there's an outlier in the data the color may seem uniform because of this outlier!
+     * @note A boost rounding exception error will be thrown if used with a non dense point cloud
+     */
+    void
+    colorCloudDistances ();
+
+
+//Slots tab6- visualization
+public slots:
+    /** @brief Triggered whenever the "Save file" button is clicked */
+    void
+    saveFileButtonPressed ();
+
+    /** @brief Triggered whenever the "Load file" button is clicked */
+    void
+    loadFileButtonPressed ();
+
+    /** @brief Triggered whenever a button in the "Color on axis" group is clicked */
+    void
+    axisChosen ();
+
+    /** @brief Triggered whenever a button in the "Color mode" group is clicked */
+    void
+    lookUpTableChosen ();
+
+
+
+
+
 };
 
 #endif // VIEWER_H
